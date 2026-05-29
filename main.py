111d1685e40308928e37c4b2c53bdbcd903bc160
Abhijeet <getabhiprakash@gmail.com>
2026-05-29 23:52:56 +0530

    Ollama with Conversational History

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from datetime import datetime
-from google.genai._interactions.types import content
+import uuid
+#from anyio.lowlevel import checkpoint
+#from google.genai._interactions.types import content
 from langchain.agents import create_agent
 #from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
 from dotenv import load_dotenv
 import os
 import gradio as gr
+#Used for saving history
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 
 
 #Env Variables Updates
@@ -17,6 +22,8 @@ def get_date():
     """ Get current date """
     return datetime.now().strftime("%Y-%m-%d")
 
+conn = sqlite3.connect("chatbot_memory.db", check_same_thread=False)
+checkpoint = SqliteSaver(conn)
 
 system_prompt = """
 You're an assistance to help answer user's query.
@@ -25,15 +32,17 @@ if the user asks date, you can use the get_date tool
 """
 llm = ChatOllama(model="qwen2.5:3b")
 
-agent = create_agent(model=llm, tools=[get_date], system_prompt=system_prompt)
+agent = create_agent(model=llm, tools=[get_date], system_prompt=system_prompt, checkpointer=checkpoint)
 
-def chat(message,history):
-    response = agent.invoke({"messages": [{"role": "user", "content": message}]})
+def chat(message,history,thread_id):
+    config = {"configurable": {"thread_id" : thread_id}}
+    response = agent.invoke({"messages": [{"role": "user", "content": message}]},config)
     last_response = response['messages'][-1].content
     return last_response
 
 with gr.Blocks() as demo:
     gr.Markdown("# AI Chatbot Agent")
-    gr.ChatInterface(fn=chat)
+    thread_id = gr.State(value = lambda: str(uuid.uuid4()))
+    gr.ChatInterface(fn=chat, additional_inputs=[thread_id])
 
 demo.launch()
